86131fb2bab7fa4fc3b6d72c1dcc33406dd32713
Mohammed Junaid <junaid7mohamed@gmail.com>
2025-01-22 23:21:23 +0530

    cookie setting check

--- service/index.js
+++ service/index.js
@@ -2,8 +2,8 @@ export function setSecureCookie(res, token) {
     res.cookie("access_token", token, {
       httpOnly: true,
       secure: true,
+      sameSite: "none",
       maxAge: 24 * 60 * 60 * 1000,
-      sameSite: "None"
     });
   
     return res;
